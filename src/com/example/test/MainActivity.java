b41b06d5267a59e616cd7a0063bd2ed77b2ad1e6
gui.li <gui.li@micoelectric.com>
2017-10-26 17:09:06 +0800

    get view location

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.test;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -28,13 +30,24 @@ public class MainActivity extends Activity implements OnClickListener {
     @Override  
     public void onClick(View v) {  
         switch (v.getId()) {  
-        case R.id.start_service:  
-            Intent startIntent = new Intent(this, MyService.class);
-            startIntent.putExtra("left", 200);  
-            startIntent.putExtra("top", 200);  
-            startIntent.putExtra("right", 600);
-            startIntent.putExtra("bottom", 600);
-            startService(startIntent);
+        case R.id.start_service:
+        	Button bt = (Button) findViewById(R.id.start_service);
+        	Rect rect = new Rect();
+			int location[] = new int[2];
+			bt.getLocationOnScreen(location);
+			rect.left = location[0];
+			rect.top = location[1];
+			rect.right = rect.left + bt.getWidth();
+			rect.bottom = rect.top + bt.getHeight();
+			
+            Intent intent = new Intent(this, MyService.class);
+         
+            intent.putExtra("action", "avpaly"); 
+			intent.putExtra("left", rect.left);  
+			intent.putExtra("top", rect.top);  
+			intent.putExtra("right", rect.right);
+            intent.putExtra("bottom", rect.bottom);
+            startService(intent);
             break;  
         case R.id.stop_service:  
             Intent stopIntent = new Intent(this, MyService.class);  
